24c5820670c5b294645b49b243220aaaec344b14
house <housesellout@gmail.com>
2024-10-04 19:32:07 -0400

    … version bump

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ import {
 import DepositToVaultModal from "../components/DepositToVaultModal";
 
 // version display
-export const currentVersion = "0.26";
+export const currentVersion = "0.27";
 
 // Custom style for the search bar
 const Search = styled("div")(({ theme }) => ({
