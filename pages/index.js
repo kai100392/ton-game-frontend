6c0c5efbfee0abbbe5073c595aa115f9fd1118da
house <housesellout@gmail.com>
2024-10-04 22:40:33 -0400

    … version bump for alpha build (v0.28)

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ import {
 import DepositToVaultModal from "../components/DepositToVaultModal";
 
 // version display
-export const currentVersion = "0.27";
+export const currentVersion = "0.28";
 
 // Custom style for the search bar
 const Search = styled("div")(({ theme }) => ({
