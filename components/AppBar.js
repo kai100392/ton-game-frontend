cb6edd88c50762fac5b3191f894d1fe3abbbd75e
fricardooo <ricofrico777@gmail.com>
2024-10-16 14:47:12 +0200

    navbar

--- components/AppBar.js
+++ components/AppBar.js
@@ -4,34 +4,34 @@ import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 const MyAppBar = () => {
   return (
-    <AppBar position="fixed" className="navbar">
-      <Toolbar className="navbarContainer">
-        <Typography variant="h6" className="navbarTitle">
+    <AppBar>
+      <Toolbar>
+        <Typography>
           MyMarket
         </Typography>
-        <Box className="navbarMenu">
+        <Box>
           <Link href="/" passHref>
-            <Button className="navbarMenuItemLink">
+            <Button>
               Home
             </Button>
           </Link>
           <Link href="/markets" passHref>
-            <Button className="navbarMenuItemLink">
+            <Button>
               Markets
             </Button>
           </Link>
           <Link href="/about" passHref>
-            <Button className="navbarMenuItemLink">
+            <Button>
               About
             </Button>
           </Link>
           <Link href="/contact" passHref>
-            <Button className="navbarMenuItemLink">
+            <Button>
               Contact
             </Button>
           </Link>
         </Box>
-        <Button className="navbarButton" onClick={() => alert('Login functionality here')}>
+        <Button onClick={() => alert('Login functionality here')}>
           Login
         </Button>
       </Toolbar>
